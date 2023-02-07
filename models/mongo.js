3fd44460efa71483fca089f5a88d6143dc1a4866
Supr0en <erik.ruotsalainen1@gmail.com>
2023-02-07 16:00:07 +0200

    pipeline added

--- models/mongo.js
+++ models/mongo.js
@@ -1,11 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 
 const dbUrl = process.env.DATABASE_URI;
 
 mongoose.set('strictQuery', false);
-mongoose.connect(dbUrl).then(() => console.log('Connected to database')).catch(error => console.error(error));;
+mongoose
+    .connect(dbUrl)
+    .then(() => console.log('Connected to database'))
+    .catch((error) => console.error(error));
 
 const personSchema = new mongoose.Schema({
     name: {
@@ -13,9 +16,11 @@ const personSchema = new mongoose.Schema({
         milength: 3,
         required: true,
         validate: {
-            validator: (value) => {return value && value.length >= 3;},
-            message: props => `${props.value} is not a valid name`
-        }
+            validator: (value) => {
+                return value && value.length >= 3;
+            },
+            message: (props) => `${props.value} is not a valid name`,
+        },
     },
     number: {
         type: String,
@@ -23,16 +28,16 @@ const personSchema = new mongoose.Schema({
         required: true,
         validate: {
             validator: (value) => /\d{2,3}-\d{6}/.test(value),
-            message: props => `${props.value} is not a valid number`
-        }
-    }
+            message: (props) => `${props.value} is not a valid number`,
+        },
+    },
 });
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString();
         delete returnedObject._id;
         delete returnedObject.__v;
-    }
+    },
 });
 
 module.exports = mongoose.model('Person', personSchema);
